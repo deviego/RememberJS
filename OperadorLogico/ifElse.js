15c2a4676e8d9426b9a2154a4680b1874ad91f0b
deviego <deviego04@gmail.com>
2022-08-24 18:09:47 -0300

    avan'o em operadores

---
+++ OperadorLogico/ifElse.js
@@ -0,0 +1,11 @@
+
+let hora = 6
+
+if(hora >=  6 & hora < 10){
+    console.log('bom dia')
+
+}else if(hora >= 12 & hora < 18 ){
+    console.log("boa tarde")
+}else{
+    console.log('boa noite')
+}
